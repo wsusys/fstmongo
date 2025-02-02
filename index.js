b5d65e0990ecad36383177c895b0a7497c3ec8b3
wsusys <168636820+wsusys@users.noreply.github.com>
2025-02-02 12:03:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.put("/UpdateUserRoles", async (rqst, res) => {
 
 app.delete("/:email/:appname", async (rqst, res) => {
   try {
-    const deletedUser = await uModel.findOneAndDelete({"email": rqst.params.email,"appname":rqst.params.appname})
+    const deletedUser = await uModel.findOneAndDelete({"email": rqst.params.email, "appname":rqst.params.appname})
     res.status(201).json(deletedUser);
   } catch (err) {
     res.status(500).json({ error: err });
